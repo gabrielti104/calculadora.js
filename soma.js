a9a1888ce4f2eaa19f61a6b53481b081f94984fd
gabrielti104 <106531227+gabrielti104@users.noreply.github.com>
2022-08-24 11:43:00 -0300

    Add files via upload

---
+++ soma.js
@@ -0,0 +1,7 @@
+function somarValores(){
+    let n1 = parseInt(document.getElementById('n1').value)
+    let n2 = parseInt(document.getElementById('n2').value)
+
+    document.getElementById('resultado').innerHTML = n1 + n2;
+
+}
